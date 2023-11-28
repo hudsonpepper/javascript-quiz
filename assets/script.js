096062cb07fa1775dd2ab1755474b810b89c091c
hudsonpepper <75504341+hudsonpepper@users.noreply.github.com>
2023-11-28 16:02:11 -0500

    Edited prev/next buttons

--- assets/script.js
+++ assets/script.js
@@ -6,6 +6,7 @@ var questionEl = document.querySelector("#question");
 var quizNavEl = document.querySelector("#question-navigation")
 var prevEl = document.querySelector("#previous");
 var nextEl = document.querySelector("#next");
+var portionEl = document.querySelector("#portion-done");
 var ans1El = document.querySelector("#answer1");
 var ans2El = document.querySelector("#answer2");
 var ans3El = document.querySelector("#answer3");
@@ -31,9 +32,16 @@ let question2= {
   answer4: "sample answer 4: cows don't sleep",
   correctAnswer: 2
 }
-let qArr = [question1, question2]
+let question3 = {
+  question: "Q3: do robot cows dream of electric milk?",
+  answer1: "sample answer 1: no...",
+  answer2: "sample answer 2: yes!",
+  answer3: "sample answer 3: this is a stupid question.",
+  answer4: "sample answer 4: cows don't sleep",
+  correctAnswer: 3
+}
+let qArr = [question1, question2, question3];
 var secondsLeft = 60;
-
 
 
 quizEl.addEventListener("click", function(event) {
@@ -84,12 +92,25 @@ function setTime() {
 }
 //setTime()
 function loadQuestion(num, qArray) {
+  if (num == 0) {
+    prevEl.classList.add("hidden");
+    nextEl.classList.remove("hidden");
+  }
+  else if (num == qArray.length - 1) {
+    nextEl.classList.add("hidden");
+    prevEl.classList.remove("hidden");
+  }
+  else {
+    prevEl.classList.remove("hidden");
+    nextEl.classList.remove("hidden");
+  }
   questionEl.textContent = qArray[num].question;
   ans1El.textContent = qArray[num].answer1;
   ans2El.textContent = qArray[num].answer2;
   ans3El.textContent = qArray[num].answer3;
   ans4El.textContent = qArray[num].answer4;
-  
+  let portionMessage = (questionNumber + 1) + " / " + qArr.length;
+  portionEl.textContent = portionMessage;
 }
 function endQuiz() {
   //ToDo: turn the quiz to the highscore section
