d8e2d56bb30d2f5129fd6751c86696273d51bcc7
hudsonpepper <75504341+hudsonpepper@users.noreply.github.com>
2023-11-29 22:48:08 -0500

    Added Comments

--- assets/js/scoreboard-script.js
+++ assets/js/scoreboard-script.js
@@ -1,23 +1,28 @@
+// Nav Bar ID Tags
 var newGameEl = document.querySelector("#new-game");
 var resetEl = document.querySelector("#reset");
+
+// Ordered List ID Tags
 var perfListEl = document.querySelector("#perfect");
 var impListEl = document.querySelector("#imperfect");
 
+// New Game Event Listener: Takes you to index.html
 newGameEl.addEventListener("click", function() {
-  console.log("Click new game");
   document.location.href="./index.html"
 });
 
+// Reset Scores Event Listener: Resets Scores and Reloads Page
 resetEl.addEventListener("click", function(event) {
-  let element = event.target;
-  console.log("Click reset scores");
   localStorage.removeItem("Score-History");
   window.location.reload();
 });
 
+// Function to Insert the Most Recent Score into relevant score array
 function insertScore() {
+  // IF: Check to Make sure Score History doesn't already have it loaded in
   if((!scoreHistory.perfectArray.includes(recentScore))&&(!scoreHistory.imperfectArray.includes(recentScore)))
   {
+    // Checks Which array to push the new Score to:
     if(recentScore.isPerfectScore){
       scoreHistory.perfectArray.push(recentScore);
     }
@@ -25,49 +30,64 @@ function insertScore() {
       scoreHistory.imperfectArray.push(recentScore);
     }
   }
+  // Sorts the Arrays by relevant stats to ensure proper order once written onto scoreboard
   scoreHistory.perfectArray = scoreHistory.perfectArray.sort(function(a, b) {return b.finalTime - a.finalTime});
   scoreHistory.imperfectArray = scoreHistory.imperfectArray.sort(function(a, b) {return b.numCorrect - a.numCorrect})
-  
-  console.log(scoreHistory);
+  // Reinitializing localStorage Score-History so it contains new score
   localStorage.setItem("Score-History", JSON.stringify(scoreHistory));
 }
   
+// Function to Generate proper scoreboard
 function writeScoreboard() {
+  // Writes Message for each element inside perfectArray
   scoreHistory.perfectArray.forEach( function(currentValue) {
+    // Makes li element for each score, and appends it to ol
     let node = document.createElement("li");
     node.textContent = currentValue.initials + ": " + currentValue.finalTime + " seconds remaining";
     if(currentValue == recentScore) {
+      // Highlights Last Score in Red to make it easily visible. 
       node.style.color = "#d9534f";
     }
     perfListEl.appendChild(node);
   })
+
+  // Writes Message for each element inside imperfectArray
   scoreHistory.imperfectArray.forEach( function(currentValue) {
+    // Makes li element for each score, and appends it to ol
     let node = document.createElement("li");
     node.textContent = currentValue.initials + ": " + currentValue.numCorrect + " / " + currentValue.numQuestions;
     if(currentValue == recentScore) {
+      // Highlights Last Score in Red to make it easily visible. 
       node.style.color = "#d9534f";
     }
     impListEl.appendChild(node);
   })
 }
+
+// Gets scoreHistory from Local Memory
 var scoreHistory = localStorage.getItem("Score-History");
+// If it doesn't exist: make proper scoreHistory object
 if(scoreHistory === null) {
   scoreHistory = {
     perfectArray: [],
     imperfectArray: []
   }
+  // Initialize scoreHistory in local memory
   localStorage.setItem("Score-History", JSON.stringify(scoreHistory));
 }
+// Format scoreHistory from local memory to proper object form
 else {
   scoreHistory = JSON.parse(scoreHistory);
 }
 
+// Gets recent Score if it exists
 var recentScore = localStorage.getItem("Most-Recent");
 if (!(recentScore === null)){
+  // If it does exist: stores it as a variable, deletes localStorage, and inserts it into scoreHistory
   recentScore = JSON.parse(recentScore);
   localStorage.removeItem("Most-Recent");
   insertScore();
 }
 
-
+// Calls writeScoreboard function to populate ordered list.
 writeScoreboard();
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,38 @@
 // Add relavent HTML IDs as JS variables
+  // Nav Bar IDs
 var timeEl = document.querySelector("#timer");
 var highscoreEl = document.querySelector("#highscores");
+  // 'Start Quiz' IDs
 var welcomeEl = document.querySelector("#pageLoad");
 var defaultEl = document.querySelector("#defaultTime");
 var deductionEl = document.querySelector("#deductionTime")
 var startEl = document.querySelector("#startQuiz");
+  // Quiz IDs
 var quizEl = document.querySelector("#quiz");
-var questionEl = document.querySelector("#question");
+    // Quiz Nav IDs
 var quizNavEl = document.querySelector("#question-navigation")
 var prevEl = document.querySelector("#previous");
 var nextEl = document.querySelector("#next");
 var portionEl = document.querySelector("#portion-done");
+    // Question IDs
+var questionEl = document.querySelector("#question");
 var ans1El = document.querySelector("#answer1");
 var ans2El = document.querySelector("#answer2");
 var ans3El = document.querySelector("#answer3");
 var ans4El = document.querySelector("#answer4");
 var respEl = document.querySelector("#response");
+      // Answer IDs in Array Format
+var answers = [ans1El, ans2El, ans3El, ans4El];
+    // Quiz Done IDs
 var scoreboardEl = document.querySelector("#scoreboard");
 var scoreEl = document.querySelector("#score");
 var initialsEl = document.querySelector("#initials");
 var confirmEl = document.querySelector("#confirm");
 
-var answers = [ans1El, ans2El, ans3El, ans4El];
+
 // Variable Initializations
 var questionNumber = 0;
+  // Question Array
 let qArr = [
   {
     question: "Which is a correct for-loop in JavaScript?",
@@ -114,6 +123,7 @@ var defaultTime = 60;
 var secondsDeducted = 5;
 var secondsLeft = defaultTime;
 var quizDone = false;
+  // Results Obj: will be stored in Local Memory
 var quizResults = {
   initials: "",
   numCorrect: 0,
@@ -121,59 +131,67 @@ var quizResults = {
   finalTime: 0,
   isPerfectScore: false
 };
-var numCorrect = 0;
 
 // EventListener for "View HighScore"
 highscoreEl.addEventListener("click", function(event) {
-  document.location.href="scoreboard.html"
-  "./scoreboard.html"
+  document.location.href="./scoreboard.html";
 })
 
-// EventListener for the Submit button for initials
+// EventListener for the Submit button for "Enter initials"
 confirmEl.addEventListener("click", function(event) {
   event.preventDefault();
   quizResults.initials = initialsEl.value;
+  // Alert Popup for if they submit before entering initials
   if(quizResults.initials == "") {
     document.querySelector("#alert").classList.remove("hidden");
   }
+  // Stores quizResults in localStorage and changes page to scoreboard.html
   else{
-  console.log("quizResults final: ", quizResults);
   localStorage.setItem("Most-Recent", JSON.stringify(quizResults));
-  document.location.href="./scoreboard.html"
+  document.location.href="./scoreboard.html";
   }
 })
 
+// JS Formatting for PageLoad
 defaultEl.textContent = defaultTime;
 deductionEl.textContent = secondsDeducted;
-startEl.addEventListener("click", startQuiz)
+startEl.addEventListener("click", startQuiz);
 
 // EventListener for Answering a question
 quizEl.addEventListener("click", function(event) {
   let element = event.target;
+  // Ensures both that they clicked on a card, and the quiz is still going
   if (element.classList.contains("card") && !quizDone) {
     var answerChoice = element.getAttribute("data-number");
+    // If the question has not yet been clicked, then log that it has
     if(!qArr[questionNumber].isQuestionAnswered[answerChoice -1]){
       qArr[questionNumber].isQuestionAnswered[answerChoice -1] = true;
+      // If answer is correct, add appropriate text, color, class
       if (answerChoice == qArr[questionNumber].correctAnswer) {
         quizResults.numCorrect++;
         respEl.textContent = "Correct! You have found " + quizResults.numCorrect + " / " + qArr.length + " correct answers.";
+        // Checks if this all questions have been answered correctly
         if(quizResults.numCorrect == qArr.length) {
+          // if so, then end the quiz and note that they got a perfect score
           quizDone = true;
           quizResults.isPerfectScore = true;
         }
         element.classList.add("correct");
         element.classList.remove("isValid");
+        // Disable all other answers for this question.
         for(let i = 0; i < answers.length; i++) {
           if ((!answers[i].classList.contains("correct")) && (!answers[i].classList.contains("incorrect")) ) {
             answers[i].classList.add("disabled");
             answers[i].classList.remove("isValid");
           }
         }
+        // Go to the next question if not the last question
         if (questionNumber != qArr.length -1) {
           questionNumber++;
           loadQuestion(questionNumber,qArr)
         }
       }
+      // Else, if they got it wrong, change message, time, and class to reflect it.
       else if (!element.classList.contains("disabled")) {
         respEl.textContent = "You got it wrong. Deducting 5 seconds from remaining time."
         secondsLeft-=secondsDeducted;
@@ -189,23 +207,25 @@ quizEl.addEventListener("click", function(event) {
 quizNavEl.addEventListener("click", function(event) {
   event.stopPropagation();
   let element = event.target;
+  // Takes in the data-number from the next and prev buttons and iterates or decrements question number accordingly.
   if (element.classList.contains("card")) {
     questionNumber += Number(element.getAttribute("data-number"));
     loadQuestion(questionNumber,qArr);
   }
 })
-
+// Display seconds left on the time box at the top
 timeEl.textContent = "Time: " + secondsLeft;
 
 //Adds Timer Functionality
 function setTime() {
   // Sets interval in variable
   var timerInterval = setInterval(function() {
+    // Assuming the quiz is not yet done: reduce time, change time display
     if(!quizDone){
     secondsLeft--;
     timeEl.textContent = "Time: " + secondsLeft;
-
     }
+    // If quiz is done or time has run out, end the quiz, reload questions to reflect it, store final time, and clear the time interval
     if(secondsLeft <= 0 || quizDone) {
       if(secondsLeft < 0) {
         secondsLeft = 0;
@@ -224,6 +244,7 @@ function setTime() {
 
 // function to deal with loadingQuestion Data
 function loadQuestion(num, qArray) {
+  // Logic as to if prev and next buttons should display given question number
   if (num == 0) {
     prevEl.classList.add("hidden");
     nextEl.classList.remove("hidden");
@@ -236,12 +257,15 @@ function loadQuestion(num, qArray) {
     prevEl.classList.remove("hidden");
     nextEl.classList.remove("hidden");
   }
+  // Logic to display correct question
   questionEl.textContent = "Q"+(questionNumber+1)+": " + qArray[num].question;
   ans1El.textContent = qArray[num].answer1;
   ans2El.textContent = qArray[num].answer2;
   ans3El.textContent = qArray[num].answer3;
   ans4El.textContent = qArray[num].answer4;
+  // Logic for correct card class formatting given question and answer history and if quiz is ongoing
   for (let i = 0; i < answers.length; i++) {
+    // Default Behavior
     if ((!qArray[num].isQuestionAnswered[i]) && (!qArray[num].isGuessedCorrect()) && (!quizDone)) {
     answers[i].classList.remove("correct");
     answers[i].classList.remove("incorrect");
@@ -249,12 +273,14 @@ function loadQuestion(num, qArray) {
     answers[i].classList.add("isValid");
     }
     else if (qArray[num].isQuestionAnswered[i]){
+      // Answer Correct Behavior
       if (i == qArray[num].correctAnswer - 1) {
         answers[i].classList.add("correct");
         answers[i].classList.remove("incorrect");
         answers[i].classList.remove("disabled");
         answers[i].classList.remove("isValid");
       }
+      // Wrong Answer Behavior
       else {
         answers[i].classList.remove("correct");
         answers[i].classList.add("incorrect");
@@ -262,6 +288,7 @@ function loadQuestion(num, qArray) {
         answers[i].classList.remove("isValid");
       }
     }
+    // Can no longer Answer Behavior
     else {
       answers[i].classList.remove("correct");
       answers[i].classList.remove("incorrect");
@@ -269,35 +296,42 @@ function loadQuestion(num, qArray) {
       answers[i].classList.remove("isValid");
     }
   }
+  // Display "question number" / "total questions" between question nav buttons
   let portionMessage = (questionNumber + 1) + " / " + qArr.length;
   portionEl.textContent = portionMessage;
 }
 
 // function to do end of quiz 
 function endQuiz() {
+  // Perfect Score Message
   if(quizResults.isPerfectScore) {
     scoreEl.textContent = "You got a perfect score with " + quizResults.finalTime + " seconds remaining"
   }
+  // Imperfect Score Message
   else {
     scoreEl.textContent = "You were only able to find " + quizResults.numCorrect + " / " + quizResults.numQuestions + " correct answers before time ran out."
   }
+  // Show initial form 
   scoreboardEl.classList.remove("hidden");
 }
 
 
-//function to 
+//function to start the quiz
 function startQuiz() {
-  
+  // Initialize Time and Question Number
   secondsLeft = defaultTime;
   questionNumber = 0;
+  // Display Quiz
   welcomeEl.classList.add("hidden");
   quizEl.classList.remove("hidden");
   scoreboardEl.classList.add("hidden");
+  // Reset Question History
   for(let i = 0; i < qArr.length; i++) {
     qArr[i].isQuestionAnswered = [false, false, false, false];
   }
+  // Load first Question
   loadQuestion(0,qArr)
   quizDone = false;
-  numCorrect = 0;
+  // Start Timer
   setTime();
 }
